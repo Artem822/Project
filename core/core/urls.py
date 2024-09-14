0c74035dd414a76a187c354d13c9c7993de2b15f
Artem <olja.lelja62@gmail.com>
2024-09-14 14:05:33 +0300

    boostrap added

--- core/core/urls.py
+++ core/core/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from project import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.test.as_view())
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/project/views.py
+++ core/project/views.py
@@ -1,3 +1,10 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
+class test(TemplateView):
+    template_name = 'base.html'
+    
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        return super().get_context_data(**kwargs)
